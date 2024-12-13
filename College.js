8eedf1e98dc4b234e48438fca90affe6a2008139
vecodesh <727723eucs268@skcet.ac.in>
2024-12-14 02:45:58 +0530

    Deleted college1.js

--- College.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-class College extends Component{
-    render(){
-        return(
-                <p>in SKCET</p>
-        )
-    }
-}
-export default College;
